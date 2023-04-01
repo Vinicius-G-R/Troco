10f02e05bae5c87f6cf7064ed8b8ae37f7958ebc
Vinicius-g-r <viniciussouza26032005@gmail.com>
2023-03-31 22:43:09 -0300

    Terminei

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿decimal ValorCompra, ValorPago, ValorTroco;
+
+Console.Write("Digite o Valor da compra: ");
+ValorCompra = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite o Valor pago: ");
+ValorPago = Convert.ToDecimal(Console.ReadLine());
+
+ValorTroco = ValorPago - ValorCompra;
+
+Console.WriteLine($"Seu troco é {ValorTroco}"); 
